30490c4527b9aec5f54db3c560321ed7b9785c8f
kewineic <kewin.ferreira@hotmail.com>
2020-09-01 23:14:23 -0300

    refatored atendimento CRUD for graphql queries

--- graphql/resolvers/atendimento.js
+++ graphql/resolvers/atendimento.js
@@ -3,14 +3,14 @@ const Atendimentos = new Operations('atendimento');
 
 const resolvers = { 
   Query: {
-    atendimentos: () => Atendimentos.lista()
-    // atendimento: (root, { id }) => Atendimentos.buscaPorId(id)
+    atendimentos: () => Atendimentos.lista(),
+    atendimento: (root, { id }) => Atendimentos.buscaPorId(id)
   },
 
   Mutation: {
-    addAtendimento: (root, params) => Atendimentos.adiciona(params)
-    // updateAtendimento: (root, params) => Atendimentos.atualiza(params),
-    // deleteAtendimento: (root, { id }) => Atendimentos.deleta(id)
+    addAtendimento: (root, params) => Atendimentos.adiciona(params),
+    updateAtendimento: (root, params) => Atendimentos.atualiza(params),
+    deleteAtendimento: (root, { id }) => Atendimentos.deleta(id)
   }
 }
 
--- infraestrutura/crud/atendimento.js
+++ infraestrutura/crud/atendimento.js
@@ -3,7 +3,31 @@ const { atendimento } = require('.');
 
 class Atendimento {
   lista() {
-    const sql = 'SELECT Atendimentos.id, Atendimentos.data, Atendimentos.status, Atendimentos.observacoes, Pets.id as petId, Pets.nome as petNome, Pets.tipo as petTipo, Pets.observacoes as petObservacoes, Clientes.id as clienteId, Clientes.nome as clienteNome, Clientes.cpf as clienteCpf, Servicos.id as servicoId, Servicos.nome as servicoNome, Servicos.preco as servicoPreco, Servicos.descricao as servicoDescricao FROM Atendimentos INNER JOIN Clientes INNER JOIN Pets INNER JOIN Servicos WHERE Atendimentos.clienteId = Clientes.id AND Atendimentos.petId = Pets.id AND Atendimentos.servicoId = Servicos.id'
+    const sql = `
+      SELECT 
+        Atendimentos.id, Atendimentos.data, 
+        Atendimentos.status, 
+        Atendimentos.observacoes, 
+        Pets.id as petId,
+        Pets.nome as petNome, 
+        Pets.tipo as petTipo, 
+        Pets.observacoes as petObservacoes, 
+        Clientes.id as clienteId, 
+        Clientes.nome as clienteNome, 
+        Clientes.cpf as clienteCpf, 
+        Servicos.id as servicoId, 
+        Servicos.nome as servicoNome, 
+        Servicos.preco as servicoPreco, 
+        Servicos.descricao as servicoDescricao 
+      FROM 
+        Atendimentos INNER JOIN Clientes INNER JOIN Pets INNER JOIN Servicos 
+      WHERE 
+        Atendimentos.clienteId = Clientes.id 
+      AND 
+        Atendimentos.petId = Pets.id 
+      AND 
+        Atendimentos.servicoId = Servicos.id
+    `
   
     return executaQuery(sql).then(atendimentos => {
       return atendimentos.map(atendimento => ({
@@ -32,15 +56,65 @@ class Atendimento {
     });
   }
 
-  buscaPorId(res, id) {
-    const sql = `SELECT * FROM Atendimentos WHERE id=${parseInt(id)}`
+  buscaPorId(id) {
+    const sql = `
+    SELECT 
+      Atendimentos.id, Atendimentos.data, 
+      Atendimentos.status, 
+      Atendimentos.observacoes, 
+      Pets.id as petId,
+      Pets.nome as petNome, 
+      Pets.tipo as petTipo, 
+      Pets.observacoes as petObservacoes, 
+      Clientes.id as clienteId, 
+      Clientes.nome as clienteNome, 
+      Clientes.cpf as clienteCpf, 
+      Servicos.id as servicoId, 
+      Servicos.nome as servicoNome, 
+      Servicos.preco as servicoPreco, 
+      Servicos.descricao as servicoDescricao 
+    FROM 
+      Atendimentos INNER JOIN Clientes INNER JOIN Pets INNER JOIN Servicos 
+    WHERE 
+      Atendimentos.clienteId = Clientes.id 
+    AND 
+      Atendimentos.petId = Pets.id 
+    AND 
+      Atendimentos.servicoId = Servicos.id
+    AND 
+      Atendimentos.id = ${id}
+  `
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(atendimentos => (
+      {
+        id: atendimentos[0].id,
+        data: atendimentos[0].data,
+        status: atendimentos[0].status,
+        observacoes: atendimentos[0].observacoes,
+        cliente: {
+          id: atendimentos[0].clienteId,
+          nome: atendimentos[0].clienteNome,
+          cpf: atendimentos[0].clienteCpf
+        },
+        pet: {
+          id: atendimentos[0].petId,
+          nome: atendimentos[0].petNome,
+          tipo: atendimentos[0].petTipo,
+          observacoes: atendimentos[0].petObservacoes
+        },
+        servico: {
+          id: atendimentos[0].servicoId,
+          nome: atendimentos[0].servicoNome,
+          preco: atendimentos[0].servicoPreco,
+          descricao: atendimentos[0].servicoDescricao
+        }
+      }
+    ));
   }
 
   adiciona(item) {
-    const { clienteId, petId, servicoId, status, observacoes } = item
-    const data = new Date().toLocaleDateString()
+    const { clienteId, petId, servicoId, status, observacoes } = item;
+    const data = new Date().toLocaleDateString();
 
     const sql = `INSERT INTO Atendimentos(
       clienteId, 
@@ -79,19 +153,58 @@ class Atendimento {
     });
   }
 
-  atualiza(res, novoItem, id) {
-    const { cliente, pet, servico, status, observacoes } = item
-    const data = new Date.toLocaleDateString()
+  atualiza(novoItem) {
+    const {id, clienteId, petId, servicoId, status, observacoes } = novoItem
+    const data = new Date().toLocaleDateString();
   
-    const sql = `UPDATE Atendimentos SET clienteId=${cliente}, petId=${pet}, servicoId=${servico}, data='${data}', status='${status}' observacoes='${observacoes}' WHERE id=${id}`
+    const sql = `
+      UPDATE 
+        Atendimentos 
+      SET 
+        clienteId=${clienteId}, 
+        petId=${petId}, 
+        servicoId=${servicoId}, 
+        data='${data}', 
+        status='${status}',
+        observacoes='${observacoes}' 
+      WHERE 
+        id=${id};
+      SELECT * FROM 
+        Clientes
+      WHERE
+        Clientes.id = ${clienteId};
+      SELECT * FROM
+        Pets
+      WHERE 
+        Pets.id = ${petId};
+      SELECT * FROM
+        Servicos 
+      WHERE Servicos.id = ${servicoId}
+    `
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then(resposta => 
+      {
+        const cliente = resposta[1][0];
+        const pet = resposta[2][0];
+        const servico = resposta[3][0];
+
+        return ({
+            id, 
+            data,
+            status,
+            observacoes,
+            cliente,
+            pet,
+            servico
+        });
+      }
+    );
   }
 
-  deleta(res, id) {
+  deleta(id) {
     const sql = `DELETE FROM Atendimentos WHERE id=${id}`
 
-    executaQuery(res, sql)
+    return executaQuery(sql).then( () => id)
   }
 }
 
